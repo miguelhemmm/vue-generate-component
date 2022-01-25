089861907d5e029cfbaf11dedc4720db95f37d32
Miguel Angel Hernandez Melo <miguel.hernandez1@arrivia.com>
2022-01-25 11:39:46 -0600

    set custom path for builds

--- vidanta/index.vue
+++
@@ -1,4 +0,0 @@
-<template src="./vidanta.html"></template>
-<script src="./vidanta.js"></script>
-<style src="./vidanta.scss" scoped lang="scss"></style>
-
--- vidanta/main.js
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue';
-import Index from './index.vue';
-import '../../assets/styles/main.scss';
-
-createApp(Index).mount('#app');
